6ab964817d98d6b054a0ec24f393944470cde614
reata <reddevil.hjw@gmail.com>
2019-05-22 22:42:20 +0800

    chore: add twine to requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import setuptools
 
 from sqllineage import name, version
 
-print(setuptools.find_packages())
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -15,10 +14,11 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/reata/sqllineage",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("tests",)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires=">=3.5",
 )
