d80f93bcdcdd4d4b9120ba8c6a0159b7e0e8ba26
reata <reddevil.hjw@gmail.com>
2020-03-23 21:38:57 +0800

    fix: assertion error when a comment is placed before table name (#38)

--- tests/test_select.py
+++ tests/test_select.py
@@ -30,6 +30,14 @@ def test_select_with_comment():
     helper("SELECT -- comment1\n col1 FROM tab1", {"tab1"})
 
 
+def test_select_with_comment_after_from():
+    helper("SELECT col1\nFROM  -- comment\ntab1", {"tab1"})
+
+
+def test_select_with_comment_after_join():
+    helper("select * from tab1 join --comment\ntab2 on tab1.x = tab2.x", {"tab1", "tab2"})
+
+
 def test_select_keyword_as_column_alias():
     # here `as` is the column alias
     helper("SELECT 1 `as` FROM tab1", {"tab1"})
