f8d7aedf289863779449317f502364b785ccdc18
reata <reddevil.hjw@gmail.com>
2020-04-11 15:47:28 +0800

    docs: add Python 3.8 classifier

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython"
     ],
     python_requires=">=3.5",
