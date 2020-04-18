ac7f4b15e846679ad21dc0cc677584ccb9aeaa2a
reata <reddevil.hjw@gmail.com>
2020-04-18 17:25:55 +0800

    test: test for eq and hash method for Database and Table

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,3 +4,10 @@ from sqllineage.models import Database, Table
 def test_repr_dummy():
     assert repr(Database())
     assert repr(Table(""))
+
+
+def test_hash_eq():
+    assert Database("a") == Database("a")
+    assert len({Database("a"), Database("a")}) == 1
+    assert Table("a") == Table("a")
+    assert len({Table("a"), Table("a")}) == 1
