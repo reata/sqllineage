6d7cbecc817db5145df0d0082cdd646ef2bdd495
reata <reddevil.hjw@gmail.com>
2020-07-18 15:32:19 +0800

    fix: self dependent query exclusion from temporary table (#56)
    * feat: cli verbose option to show statement level lineage
    
    * test: dummy test for combiner, runner and core
    
    * fix: do not mark table in self dependent query as temporary table
    
    * test: fix mypy checking caused coverage issue
    
    * test: more test cases for models

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,5 +2,5 @@ from sqllineage.core import LineageResult
 
 
 def test_dummy():
-    assert str(LineageResult())
+    assert str(LineageResult()) == repr(LineageResult())
     assert LineageResult() + LineageResult() is not None
