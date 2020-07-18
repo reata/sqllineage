29ba4fb0a17dbad1b8b1a484b4e20b91659a7102
reata <reddevil.hjw@gmail.com>
2020-07-18 18:50:02 +0800

    test: coverage for intermediate table logic

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -6,7 +6,9 @@ from sqllineage.runner import LineageRunner, main
 def test_dummy():
     assert str(
         LineageRunner(
-            "select col1, col2, col3, col4, col5, col6 from tab1;", verbose=True
+            """insert into tab2 select col1, col2, col3, col4, col5, col6 from tab1;
+insert overwrite table tab3 select * from tab2""",
+            verbose=True,
         )
     )
     with pytest.raises(SystemExit) as e:
