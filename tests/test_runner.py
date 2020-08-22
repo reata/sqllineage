7434b7d443e1f7c42d3af2cb110302345aa2fb77
reata <reddevil.hjw@gmail.com>
2020-08-22 21:12:31 +0800

    test: drawing test from -g option

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,11 +1,13 @@
 import pathlib
+from unittest.mock import patch
 
 import pytest
 
 from sqllineage.runner import LineageRunner, main
 
 
-def test_dummy():
+@patch("matplotlib.pyplot.show")
+def test_dummy(_):
     assert str(
         LineageRunner(
             """insert into tab2 select col1, col2, col3, col4, col5, col6 from tab1;
@@ -17,6 +19,13 @@ insert overwrite table tab3 select * from tab2""",
     main(["-e", "select * from dual"])
     main(["-f", __file__])
     main(["-e", "select * from dual", "-f", __file__])
+    main(
+        [
+            "-e",
+            "insert overwrite table tab1 select * from tab1 union select * from tab2",
+            "-g",
+        ]
+    )
 
 
 def test_file_exception():
