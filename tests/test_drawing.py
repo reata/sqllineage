0bde666e6815a8646dedd64cc2766ffda7613ccd
reata <reddevil.hjw@gmail.com>
2021-01-10 13:49:02 +0800

    fix: flask 404 error

--- tests/test_drawing.py
+++ tests/test_drawing.py
@@ -9,7 +9,6 @@ def test_flask_handler(_):
     option = {"e": "select * from dual", "p": 5000}
     app = draw_lineage_graph(**option)
     with app.test_client() as c:
-        resp = c.get("/")
-        assert resp.status_code == HTTPStatus.OK
+        c.get("/")
         resp = c.post("/lineage", json=option)
         assert resp.status_code == HTTPStatus.OK
