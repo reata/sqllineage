d25108518e5a6f05ca6cfac5c8374a4381a46187
reata <reddevil.hjw@gmail.com>
2021-06-14 15:13:23 +0800

    feat: add support for database.schema.table as identifier name (#154)

--- sqllineage/models.py
+++ sqllineage/models.py
@@ -1,6 +1,8 @@
 import warnings
 
 from sqlparse.sql import Identifier
+from sqlparse.tokens import Punctuation
+from sqlparse.utils import imt
 
 from sqllineage.exceptions import SQLLineageException
 from sqllineage.helpers import escape_identifier_name
@@ -41,17 +43,18 @@ class Table:
         :param name: table name
         :param schema: schema as defined by :class:`Schema`
         """
-        if len(name.split(".")) == 2:
-            schema_name, table_name = name.split(".")
+        if "." not in name:
+            self.schema = schema
+            self.raw_name = escape_identifier_name(name)
+        else:
+            schema_name, table_name = name.rsplit(".", 1)
+            if len(schema_name.split(".")) > 2:
+                # allow db.schema as schema_name, but a.b.c as schema_name is forbidden
+                raise SQLLineageException("Invalid format for table name: %s.", name)
             self.schema = Schema(schema_name)
             self.raw_name = escape_identifier_name(table_name)
             if schema:
                 warnings.warn("Name is in schema.table format, schema param is ignored")
-        elif "." not in name:
-            self.schema = schema
-            self.raw_name = escape_identifier_name(name)
-        else:
-            raise SQLLineageException("Invalid format for table name: %s.", name)
 
     def __str__(self):
         return f"{self.schema}.{self.raw_name.lower()}"
@@ -67,12 +70,27 @@ class Table:
 
     @staticmethod
     def create(identifier: Identifier):
-        schema = (
-            Schema(identifier.get_parent_name())
-            if identifier.get_parent_name() is not None
-            else Schema()
+        # rewrite identifier's get_real_name method, by matching the last dot instead of the first dot, so that the
+        # real name for a.b.c will be c instead of b
+        dot_idx, _ = identifier._token_matching(
+            lambda token: imt(token, m=(Punctuation, ".")),
+            start=len(identifier.tokens),
+            reverse=True,
         )
-        return Table(identifier.get_real_name(), schema)
+        real_name = identifier._get_first_name(dot_idx, real_name=True)
+        # rewrite identifier's get_parent_name accordingly
+        parent_name = (
+            "".join(
+                [
+                    escape_identifier_name(token.value)
+                    for token in identifier.tokens[:dot_idx]
+                ]
+            )
+            if dot_idx
+            else None
+        )
+        schema = Schema(parent_name) if parent_name is not None else Schema()
+        return Table(real_name, schema)
 
 
 class Partition:
