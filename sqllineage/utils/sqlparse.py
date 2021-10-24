f965262c04ea083195724243a54b3c084a929013
reata <reddevil.hjw@gmail.com>
2021-10-24 21:14:33 +0800

    refactor: get_parameters as a separate function in utils

--- sqllineage/utils/sqlparse.py
+++ sqllineage/utils/sqlparse.py
@@ -1,5 +1,5 @@
 from sqlparse import tokens as T
-from sqlparse.sql import Parenthesis, TokenList
+from sqlparse.sql import Function, Parenthesis, TokenList
 
 
 def is_subquery(token: TokenList) -> bool:
@@ -9,3 +9,11 @@ def is_subquery(token: TokenList) -> bool:
         if sub_token is not None:
             flag = True
     return flag
+
+
+def get_parameters(token: Function):
+    """
+    This is a replacement for sqlparse.sql.Function.get_parameters(), which produces problematic result for:
+        if(col1 = 'foo' AND col2 = 'bar', 1, 0)
+    """
+    return token.tokens[-1].get_sublists()
