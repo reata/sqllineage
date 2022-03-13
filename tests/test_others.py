3a8db5d8db6035c67260fcfa4071fcf569b2924e
reata <reddevil.hjw@gmail.com>
2022-03-13 21:26:01 +0800

    fix: source table name and select statement both in parentheses

--- tests/test_others.py
+++ tests/test_others.py
@@ -53,6 +53,12 @@ def test_create_as_with_parenthesis_around_table_name():
     )
 
 
+def test_create_as_with_parenthesis_around_both():
+    assert_table_lineage_equal(
+        "CREATE TABLE tab1 AS (SELECT * FROM (tab2))", {"tab2"}, {"tab1"}
+    )
+
+
 def test_create_like():
     assert_table_lineage_equal("CREATE TABLE tab1 LIKE tab2", {"tab2"}, {"tab1"})
 
