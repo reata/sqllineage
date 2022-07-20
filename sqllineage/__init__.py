c0bf3a37d8a6c7be7a1641cebb0484ca54845f37
reata <reddevil.hjw@gmail.com>
2022-07-20 21:14:25 +0800

    fix: exception when parsing certain lateral view SQL

--- sqllineage/__init__.py
+++ sqllineage/__init__.py
@@ -16,10 +16,23 @@ def _patch_adding_builtin_type() -> None:
     KEYWORDS["DATETIME"] = tokens.Name.Builtin
 
 
+def _patch_updating_lateral_view_lexeme() -> None:
+    import re
+    from sqlparse.keywords import SQL_REGEX
+
+    for i, (regex, lexeme) in enumerate(SQL_REGEX):
+        if regex("LATERAL VIEW EXPLODE(col)"):
+            new_regex = r"(LATERAL\s+VIEW\s+)(OUTER\s+)?(EXPLODE|INLINE|PARSE_URL_TUPLE|POSEXPLODE|STACK|JSON_TUPLE)\b"
+            new_compile = re.compile(new_regex, re.IGNORECASE | re.UNICODE).match
+            SQL_REGEX[i] = (new_compile, lexeme)
+            break
+
+
 def _monkey_patch() -> None:
     try:
         _patch_adding_window_function_token()
         _patch_adding_builtin_type()
+        _patch_updating_lateral_view_lexeme()
     except ImportError:
         # when imported by setup.py for constant variables, dependency is not ready yet
         pass
