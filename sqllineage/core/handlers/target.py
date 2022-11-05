62160a8dfa76f17032608c7921ba7045df37c537
reata <reddevil.hjw@gmail.com>
2022-11-05 20:32:01 +0800

    fix: unnest function call with extra space in between

--- sqllineage/core/handlers/target.py
+++ sqllineage/core/handlers/target.py
@@ -10,10 +10,26 @@ from sqllineage.exceptions import SQLLineageException
 class TargetHandler(NextTokenBaseHandler):
     """Target Table Handler."""
 
-    TARGET_TABLE_TOKENS = ("INTO", "OVERWRITE", "TABLE", "VIEW", "UPDATE", "COPY")
+    TARGET_TABLE_TOKENS = (
+        "INTO",
+        "OVERWRITE",
+        "TABLE",
+        "VIEW",
+        "UPDATE",
+        "COPY",
+        "DIRECTORY",
+    )
 
     def _indicate(self, token: Token) -> bool:
-        return token.normalized in self.TARGET_TABLE_TOKENS
+        if (
+            self.indicator is True
+            and token.is_keyword
+            and token.normalized in ("IF", "NOT", "EXISTS")
+        ):
+            # special handling for CREATE TABLE IF NOT EXISTS
+            return True
+        else:
+            return token.normalized in self.TARGET_TABLE_TOKENS
 
     def _handle(self, token: Token, holder: SubQueryLineageHolder) -> None:
         if isinstance(token, Function):
@@ -40,12 +56,7 @@ class TargetHandler(NextTokenBaseHandler):
             holder.add_read(Table.of(token.right))
         elif token.ttype == Literal.String.Single:
             holder.add_write(Path(token.value))
-        else:
-            if not isinstance(token, Identifier):
-                raise SQLLineageException(
-                    "An Identifier is expected, got %s[value: %s] instead."
-                    % (type(token).__name__, token)
-                )
+        elif isinstance(token, Identifier):
             if token.token_first(skip_cm=True).ttype is Number.Integer:
                 # Special Handling for Spark Bucket Table DDL
                 pass
