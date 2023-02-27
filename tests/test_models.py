02262b813d7f68f388cc2892a93e9eb351f4e927
reata <reddevil.hjw@gmail.com>
2023-02-27 19:20:55 +0800

    test: test models using base model

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,34 +3,29 @@ from unittest.mock import Mock
 import pytest
 from sqlparse.sql import Parenthesis
 
-from sqllineage.core.exceptions import SQLLineageException
 from sqllineage.core.models import Column, Path, Schema, SubQuery, Table
 from sqllineage.core.parser.sqlfluff.models import SqlFluffColumn
-from sqllineage.core.parser.sqlparse.models import (
-    SqlParseColumn,
-    SqlParseSubQuery,
-    SqlParseTable,
-)
+from sqllineage.exceptions import SQLLineageException
 
 
 def test_repr_dummy():
     assert repr(Schema())
-    assert repr(SqlParseTable(""))
-    assert repr(SqlParseTable("a.b.c"))
-    assert repr(SqlParseSubQuery(Parenthesis(), Parenthesis().value, ""))
-    assert repr(SqlParseColumn("a.b"))
+    assert repr(Table(""))
+    assert repr(Table("a.b.c"))
+    assert repr(SubQuery(Parenthesis(), Parenthesis().value, ""))
+    assert repr(Column("a.b"))
     assert repr(Path(""))
     with pytest.raises(SQLLineageException):
-        SqlParseTable("a.b.c.d")
+        Table("a.b.c.d")
     with pytest.warns(Warning):
-        SqlParseTable("a.b", Schema("c"))
+        Table("a.b", Schema("c"))
 
 
 def test_hash_eq():
     assert Schema("a") == Schema("a")
     assert len({Schema("a"), Schema("a")}) == 1
-    assert SqlParseTable("a") == SqlParseTable("a")
-    assert len({SqlParseTable("a"), SqlParseTable("a")}) == 1
+    assert Table("a") == Table("a")
+    assert len({Table("a"), Table("a")}) == 1
 
 
 def test_of_dummy():
