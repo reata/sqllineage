f8b758970cc8e2b1547ff61f363e0dac26297ead
reata <reddevil.hjw@gmail.com>
2023-03-26 16:27:19 +0800

    feat: differentiate full and partial unparsable exception

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -13,11 +13,16 @@ def test_select_without_table():
         LineageRunner("select * from where foo='bar'")._eval()
 
 
-def test_unsupported_query_type_in_sqlfluff():
-    with pytest.raises(UnsupportedStatementException):
+def test_full_unparsable_query_in_sqlfluff():
+    with pytest.raises(InvalidSyntaxException):
         LineageRunner("WRONG SELECT FROM tab1")._eval()
 
 
 def test_partial_unparsable_query_in_sqlfluff():
     with pytest.raises(InvalidSyntaxException):
         LineageRunner("SELECT * FROM tab1 AS FULL FULL OUTER JOIN tab2")._eval()
+
+
+def test_unsupported_query_type_in_sqlfluff():
+    with pytest.raises(UnsupportedStatementException):
+        LineageRunner("CREATE UNIQUE INDEX title_idx ON films (title)")._eval()
