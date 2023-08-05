3740f903c24898164c8124b74fa2b8c0b136307b
reata <reddevil.hjw@gmail.com>
2023-08-05 19:52:37 +0800

    refactor: change edge type to str for readability

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     install_requires=[
         "sqlparse>=0.4.4",
         "networkx>=2.4",
-        "sqlfluff>=2.1.4",
+        "sqlfluff==2.1.4",
     ],
     entry_points={"console_scripts": ["sqllineage = sqllineage.cli:main"]},
     extras_require={
