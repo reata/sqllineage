e5084203309bbafbf3dbdeedb04a871b014bb1f3
reata <reddevil.hjw@gmail.com>
2023-09-02 14:28:52 +0800

    refactor: change AnalyzerContext to NamedTuple

--- sqllineage/utils/entities.py
+++ sqllineage/utils/entities.py
@@ -1,6 +1,6 @@
-from typing import Any, NamedTuple, Optional, Set, Union
+from typing import Any, List, NamedTuple, Optional, Set, Union
 
-from sqllineage.core.models import SubQuery, Table
+from sqllineage.core.models import Column, SubQuery, Table
 
 
 class SubQueryTuple(NamedTuple):
@@ -13,24 +13,10 @@ class ColumnQualifierTuple(NamedTuple):
     qualifier: Optional[str]
 
 
-class AnalyzerContext:
-    """
-    Data class to hold the analyzer context
-    """
-
-    def __init__(
-        self,
-        cte: Optional[Set[SubQuery]] = None,
-        write: Optional[Set[Union[SubQuery, Table]]] = None,
-        write_columns=None,
-    ):
-        """
-        :param cte: CTE queries that can be select from in current query context
-        :param write: table that current top-level query is writing to, subquery in case of subquery context
-        :param write_columns: columns that write table specifies, used for `INSERT INTO x (col1, col2) SELECT` syntax
-        """
-        self.cte = cte
-        self.write = write
-        if write_columns is None:
-            write_columns = []
-        self.write_columns = write_columns
+class AnalyzerContext(NamedTuple):
+    # CTE queries that can be select from in current query context
+    cte: Optional[Set[SubQuery]] = None
+    # table that current top-level query is writing to, subquery in case of subquery context
+    write: Optional[Set[Union[SubQuery, Table]]] = None
+    # columns that write table specifies, used for `INSERT INTO x (col1, col2) SELECT` syntax
+    write_columns: Optional[List[Column]] = None
