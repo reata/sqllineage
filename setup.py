9ce708ebe3baa04f9e48297dc88935c9e40f07c1
reata <reddevil.hjw@gmail.com>
2023-10-16 21:47:36 +0800

    chore: bump version to 1.4.8

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     install_requires=[
         "sqlparse>=0.4.4",
         "networkx>=2.4",
-        "sqlfluff==2.3.2",
+        "sqlfluff==2.3.3",
     ],
     entry_points={"console_scripts": ["sqllineage = sqllineage.cli:main"]},
     extras_require={
