833542014588fa3d4fb54719c58207aa1661bfd7
liuzhou <liuzhou@asiainfo.com>
2024-01-14 22:26:34 +0800

    fix: local modify

--- sqllineage/config.py
+++ sqllineage/config.py
@@ -1,81 +1,119 @@
 import os
+from collections import namedtuple
 from dataclasses import dataclass
+from enum import auto
 from typing import Any, Union
 
+from pip._internal.utils.misc import enum
+
+#
+# class SQLLineageConfigKey(enum):
+#     DIRECTORY=auto()
+#     DEFAULT_SCHEMA=auto()
+#     TSQL_NO_SEMICOLON=auto()
+#
 @dataclass
 class SQLLineageConfigValue:
     class_type: Any
     value: Union[str, bool, None]
 
-
+#
 @dataclass
-class SQLLineageConfigDef():
+class SQLLineageConfigDef:
     DIRECTORY: SQLLineageConfigValue
     DEFAULT_SCHEMA: SQLLineageConfigValue
     TSQL_NO_SEMICOLON: SQLLineageConfigValue
 
 
-class _SQLLineageConfigLoader() :
+class _SQLLineageConfigLoader:
     """
-    Load all configurable items from environment variable, otherwise fallback to default
+    Load all configurable items from config variable, otherwise fallback to default
     """
+    def __init__(self) -> None:
+        # SQLLineageConfigDef = namedtuple('SQLLineageConfigDef', ['class', 'value'])
+        self._config = {
+            "DIRECTORY": {
+                "class_type": str,
+                "value": {
+                    "additional": None,
+                    "os_env": None,
+                    "default": os.path.join(os.path.dirname(__file__), "data"),
+                },
+            },
+            "DEFAULT_SCHEMA": {
+                "class_type": str,
+                "value": {
+                    "additional": None,
+                    "os_env": None,
+                    "default": None,
+                },
+            },
+            "TSQL_NO_SEMICOLON": {
+                "class_type": bool,
+                "value": {"additional": None, "os_env": None, "default": False},
+            },
+        }
 
-    def __init__(self):
-        self._config = SQLLineageConfigDef(
-            DIRECTORY=SQLLineageConfigValue(
-                str, os.path.join(os.path.dirname(__file__), "data")
-            ),
-            DEFAULT_SCHEMA=SQLLineageConfigValue(str, None),
-            TSQL_NO_SEMICOLON=SQLLineageConfigValue(bool, False),
-        )
+        self._prefix = "SQLLINEAGE_"
+        self._len_prefix = len(self._prefix)
+
+        for item in set(
+            [self._prefix + key for key in self._config.keys()]
+        ).intersection(set(os.environ.keys())):
+            self._config[item[self._len_prefix :]] = os.environ[item]
 
     @property
     def DEFAULT_SCHEMA(self):
-        return self._config.DEFAULT_SCHEMA.value
+        for key, value in self._config["DEFAULT_SCHEMA"]["value"]:
+            if value or key == "default":
+                return value
 
     @DEFAULT_SCHEMA.setter
-    def DEFAULT_SCHEMA(self, value):
-        if value:
-            if isinstance(value, self._config.DEFAULT_SCHEMA.class_type):
-                self._config.DEFAULT_SCHEMA.value = value
-            else:
-                raise ValueError(
-                    f"DEFAULT_SCHEMA should be {self._config.DEFAULT_SCHEMA.class_type}"
-                )
+    def DEFAULT_SCHEMA(self, value: str):
+        if (
+            value and isinstance(value, self._config["DEFAULT_SCHEMA"]["class_type"])
+        ) or value is None:
+            self._config["DEFAULT_SCHEMA"]["value"]["additional"] = value
+        else:
+            raise ValueError(
+                f"DEFAULT_SCHEMA should be {self._config['DEFAULT_SCHEMA']['class_type']}"
+            )
 
     @property
     def DIRECTORY(self):
-        return self._config.DIRECTORY.value
+        for key, value in self._config["DIRECTORY"]["value"]:
+            if value or key == "default":
+                return value
 
     @DIRECTORY.setter
-    def DIRECTORY(self, value):
-        if value:
-            if isinstance(value, self._config.DEFAULT_SCHEMA.class_type):
-                self._config.DIRECTORY.value = value
-            else:
-                raise ValueError(f"DIRECTORY should be {self._config.DIRECTORY.class_type}")
+    def DIRECTORY(self, value: str):
+        if value is None:
+            self._config["DIRECTORY"]["value"]["additional"] = value
+        else:
+            assert isinstance(
+                value, self._config["DIRECTORY"]["class_type"]
+            ), ValueError(
+                f"DIRECTORY should be {self._config['DIRECTORY']['class_type']}"
+            )
+            assert os.path.isdir(value), NotADirectoryError(f"DIRECTORY does not exits")
+            self._config["DIRECTORY"]["value"]["additional"] = value
 
     @property
     def TSQL_NO_SEMICOLON(self):
-        return self._config.TSQL_NO_SEMICOLON.value
+        for key, value in self._config["TSQL_NO_SEMICOLON"]["value"]:
+            if value or key == "default":
+                return value
 
     @TSQL_NO_SEMICOLON.setter
-    def TSQL_NO_SEMICOLON(self, value):
-        if value :
-            if isinstance(value, self._config.TSQL_NO_SEMICOLON.class_type):
-                self._config.TSQL_NO_SEMICOLON.value = value
-            else:
-                raise ValueError(
-                    f"TSQL_NO_SEMICOLON should be {self._config.TSQL_NO_SEMICOLON.class_type}"
-                )
+    def TSQL_NO_SEMICOLON(self, value: bool):
+        if (
+            value and isinstance(value, self._config["TSQL_NO_SEMICOLON"]["class_type"])
+        ) or value is None:
+            self._config["TSQL_NO_SEMICOLON"]["value"]["additional"] = value
+        else:
+            raise ValueError(
+                f"TSQL_NO_SEMICOLON should be {self._config['TSQL_NO_SEMICOLON']['class_type']}"
+            )
 
 
 SQLLineageConfig = _SQLLineageConfigLoader()
-
-if __name__ == "__main__":
-    SQLLineageConfig.DIRECTORY = "xxx"
-    SQLLineageConfig.DEFAULT_SCHEMA = "ods"
-    SQLLineageConfig.TSQL_NO_SEMICOLON = "xxx"
-    print(SQLLineageConfig.DIRECTORY)
-    print(SQLLineageConfig.DEFAULT_SCHEMA)
-    print(SQLLineageConfig.TSQL_NO_SEMICOLON)
