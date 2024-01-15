b0fa8604065a59ec3726a99e4ad517a2c599135c
liuzhou <liuzhou@asiainfo.com>
2024-01-15 11:16:22 +0800

    fix: Set param from config #545 redo rebase and clean submit

--- sqllineage/config.py
+++ sqllineage/config.py
@@ -3,7 +3,7 @@ import os
 
 class _SQLLineageConfigLoader:
     """
-    Load all configurable items from environment variable, otherwise fallback to default
+    Load all configurable items from config variable, otherwise fallback to default
     """
 
     # inspired by https://github.com/joke2k/django-environ
@@ -19,18 +19,32 @@ class _SQLLineageConfigLoader:
     }
     BOOLEAN_TRUE_STRINGS = ("true", "on", "ok", "y", "yes", "1")
 
-    def __getattr__(self, item):
-        if item in self.config:
-            type_, default = self.config[item]
-            # require SQLLINEAGE_ prefix from environment variable
-            return self.parse_value(
-                os.environ.get("SQLLINEAGE_" + item, default), type_
-            )
-        else:
-            return super().__getattribute__(item)
+    def __init__(self) -> None:
+        self._init = False
+        self._config = {
+            "DIRECTORY": {
+                "class_type": str,
+                "additional": None,
+                "default": os.path.join(os.path.dirname(__file__), "data"),
+            },
+            "DEFAULT_SCHEMA": {
+                "class_type": str,
+                "additional": None,
+                "default": None,
+            },
+            "TSQL_NO_SEMICOLON": {
+                "class_type": bool,
+                "additional": None,
+                "default": False,
+            },
+        }
 
-    @classmethod
-    def parse_value(cls, value, cast):
+        self._PREFIX = "SQLLINEAGE_"
+        self._BOOLEAN_TRUE_STRINGS = ("true", "on", "ok", "y", "yes", "1")
+        self._len_prefix = len(self._PREFIX)
+        self._init = True
+
+    def parse_value(self, value, cast):
         """Parse and cast provided value
 
         :param value: Stringed value.
@@ -42,11 +56,33 @@ class _SQLLineageConfigLoader:
             try:
                 value = int(value) != 0
             except ValueError:
-                value = value.lower().strip() in cls.BOOLEAN_TRUE_STRINGS
+                value = value.lower().strip() in self._BOOLEAN_TRUE_STRINGS
         else:
             value = cast(value)
 
         return value
+
+    def __getattr__(self, item):
+        if self._config[item]["additional"] is not None:
+            return self._config[item]["additional"]
+        elif (os_env_item := self._PREFIX + item) in os.environ.keys():
+            return self.parse_value(
+                os.environ[os_env_item], self._config[item]["class_type"]
+            )
+        else:
+            return self._config[item]["default"]
+
+    def __setattr__(self, key, value):
+        if key == "_init" or not self._init:
+            super().__setattr__(key, value)
+        else:
+            if key not in [field for field in self._config.keys()]:
+                raise ValueError(f"config {key} is not support")
+
+            if value is None or isinstance(value, self._config[key]["class_type"]):
+                self._config[key]["additional"] = value
+            else:
+                raise ValueError(f"{key}:{value} class type incorrect")
 
 
 SQLLineageConfig = _SQLLineageConfigLoader()
--- tests/core/test_config.py
+++ tests/core/test_config.py
@@ -1,7 +1,21 @@
 import os
+from pathlib import Path
 from unittest.mock import patch
 
+import pytest
+
 from sqllineage.config import SQLLineageConfig
+
+
+def test_config_default_value():
+    assert (
+        SQLLineageConfig.DIRECTORY
+        == Path(os.path.dirname(__file__))
+        .parent.parent.joinpath("sqllineage", "data")
+        .__str__()
+    )
+    assert SQLLineageConfig.DEFAULT_SCHEMA is None
+    assert SQLLineageConfig.TSQL_NO_SEMICOLON is False
 
 
 @patch(
@@ -21,3 +35,37 @@ def test_config():
 
     assert type(SQLLineageConfig.TSQL_NO_SEMICOLON) is bool
     assert SQLLineageConfig.TSQL_NO_SEMICOLON is True
+
+
+@patch(
+    "os.environ",
+    {
+        "SQLLINEAGE_DIRECTORY": os.path.join(os.path.dirname(__file__), "data"),
+        "SQLLINEAGE_DEFAULT_SCHEMA": "<default>",
+        "SQLLINEAGE_TSQL_NO_SEMICOLON": "true",
+    },
+)
+def test_config_reset():
+    SQLLineageConfig.DIRECTORY = os.path.join(os.path.dirname(__file__), "")
+    assert type(SQLLineageConfig.DIRECTORY) is str
+    assert SQLLineageConfig.DIRECTORY == os.path.join(os.path.dirname(__file__), "")
+
+    SQLLineageConfig.DEFAULT_SCHEMA = "ods"
+    assert type(SQLLineageConfig.DEFAULT_SCHEMA) is str
+    assert SQLLineageConfig.DEFAULT_SCHEMA == "ods"
+
+    SQLLineageConfig.TSQL_NO_SEMICOLON = True
+    assert type(SQLLineageConfig.TSQL_NO_SEMICOLON) is bool
+    assert SQLLineageConfig.TSQL_NO_SEMICOLON is True
+
+    SQLLineageConfig.DIRECTORY = None
+    SQLLineageConfig.DEFAULT_SCHEMA = None
+    SQLLineageConfig.TSQL_NO_SEMICOLON = None
+
+
+def test_config_exception():
+    with pytest.raises(ValueError):
+        SQLLineageConfig.DIRECTORYxx = "xx"
+
+    with pytest.raises(ValueError):
+        SQLLineageConfig.DEFAULT_SCHEMA = False
