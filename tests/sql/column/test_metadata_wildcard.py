cfc55de6c1fa5e1c364de4a8e67c738a5e12d1d2
kkozhakin <kkozhakin@mail.ru>
2025-06-18 12:25:47 +0300

    fix

--- tests/sql/column/test_metadata_wildcard.py
+++ tests/sql/column/test_metadata_wildcard.py
@@ -337,10 +337,10 @@ def test_wildcard_reference_from_previous_statements(provider: MetaDataProvider)
         metadata_provider=provider,
     )
 
+
 # TODO
 # Expected Lineage: {(Column: <default>.tab_b.col_b, Column: <default>.tab_c.col_a), (Column: <default>.base.*, Column: <default>.tab_c.*)}
 # Actual Lineage: {(Column: <default>.tab_b.col_b, Column: <default>.tab_c.col_a), (Column: base.*, Column: <default>.tab_c.*)}
-
 # def test_output_consistency():
 #     sql = """INSERT INTO tab_c
 # SELECT *,
