c3d58a737b297bcc0c1529ec6e66585c6ea18713
Alex Silva <alexjbsiduff@gmail.com>
2026-05-02 01:22:33 -0300

    feat: add stored procedure lineage extraction Add ProcedureExtractor to parse CREATE PROCEDURE statements. Extracts table and column lineage from procedure body by: - Using recursive_crawl to find nested statements - Delegating to existing extractors (Select, Insert, etc.) Supports BigQuery, Oracle and T-SQL dialects.
    Tests added:
    - tests/sql/table/test_procedure_dialect_specific.py
    - tests/sql/column/test_column_procedure_dialect_specific.py
    
    Currently, SQLFluff version 4.1.0 has some degree of support for parsing stored
    procedures in the following dialects: bigquery, mariadb, mysql, oracle,
    postgres, redshift, snowflake and tsql.
    Among those dialects, only bigquery, oracle, and tsql parsed trees have enough
    elements for some lineage extraction.
    
    Dialects `mariadb` and `mysql` are **not** supported because SQLFluff can't
    cope with dynamic delimiters (https://github.com/sqlfluff/sqlfluff/issues/7081);
    Dialects `postgres` and `redshift` are **not** supported because SQLFluff
    treats the procedure body (internals) as a single string literal (`quoted_literal` type);
    Dialect `snowflake` is **not** supported because SQLFluff treats the procedure
    body (internals) as a single string literal (`udf_body` type).

---
+++ sqllineage/core/parser/sqlfluff/extractors/procedure.py
@@ -0,0 +1,33 @@
+from sqlfluff.core.parser import BaseSegment
+
+from sqllineage.core.holders import StatementLineageHolder, SubQueryLineageHolder
+from sqllineage.core.parser.sqlfluff.extractors.base import BaseExtractor
+from sqllineage.utils.entities import AnalyzerContext
+
+
+class ProcedureExtractor(BaseExtractor):
+    """
+    Stored Procedure lineage extractor.
+    """
+
+    SUPPORTED_STMT_TYPES = ["create_procedure_statement"]
+
+    def extract(
+        self,
+        statement: BaseSegment,
+        context: AnalyzerContext,
+    ) -> StatementLineageHolder:
+        holder = StatementLineageHolder()
+
+        for segment in statement.recursive_crawl("statement"):
+            holder |= self._delegate_to_extractor(segment.segments[0], context)
+        return holder
+
+    def _delegate_to_extractor(
+        self, segment: BaseSegment, context: AnalyzerContext
+    ) -> SubQueryLineageHolder:
+        for extractor_cls in BaseExtractor.__subclasses__():
+            extractor = extractor_cls(self.dialect, self.metadata_provider)
+            if extractor.can_extract(segment.type):
+                return extractor.extract(segment, context)
+        return SubQueryLineageHolder()
